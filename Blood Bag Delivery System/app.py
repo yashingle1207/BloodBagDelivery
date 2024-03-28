eecf3d3580b98b58cfd549dea1e91aac78265d1f
Yash Ingle <84223544+yashingle1207@users.noreply.github.com>
2024-03-29 03:12:21 +0530

    Update app.py

--- Blood Bag Delivery System/app.py
+++ Blood Bag Delivery System/app.py
@@ -922,14 +922,19 @@ def HospDashboard():
         return redirect(url_for('HospsignIn'))
 
 
-@app.route('/HDashboard')
-def hdash():
-    return render_template('HospitalDashboard.html')
-
-
 @app.route('/BBDashboard')
-def bbdash():
-    return render_template('BloodBankDashboard.html')
+def BBDashboard():
+    # Retrieve the registration number from the session
+    bb_reg_no = session.get('bb_reg_no')
+
+    # Check if the user is logged in
+    if bb_reg_no:
+        return render_template('BloodBankDashboard.html', bb_reg_no=bb_reg_no)
+    else:
+        # Redirect to the login page if not logged in
+        return redirect(url_for('BBsignIn'))
+
+
 
 @app.route('/AddBB')
 def addbb():
