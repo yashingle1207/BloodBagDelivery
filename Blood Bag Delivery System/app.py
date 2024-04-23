5ed3da52c346c011d51fdd87ec76cd3d017db517
Yash Ingle <84223544+yashingle1207@users.noreply.github.com>
2024-04-23 21:52:40 +0530

    Update app.py

--- Blood Bag Delivery System/app.py
+++ Blood Bag Delivery System/app.py
@@ -268,14 +268,16 @@ def payment_response():
                   
 
                 # Redirect to the success page
-                return render_template(template_name, 
+            return render_template(template_name, 
                        order_id=order_id,
                        phonepe_transaction_id=phonepe_transaction_id,
                        total_amt=total_amt,
                        timestamp=ist_timestamp,
                        blood_group=blood_group,
                        blood_component=blood_component,
-                       requested_quantity=requested_quantity)
+                       requested_quantity=requested_quantity,
+                       blood_product_price=blood_product_price)
+
 
             else:
                 # Payment failed, log and redirect to payment failure page
