6000ee62b7775b8695e5f0aaef08583993f28718
Yash Ingle <84223544+yashingle1207@users.noreply.github.com>
2024-06-07 12:19:46 +0530

    Update app.py

--- Blood Bag Delivery System/app.py
+++ Blood Bag Delivery System/app.py
@@ -1832,6 +1832,9 @@ def viewstock():
 
 @app.route('/delorder', methods=['GET'])
 def bloodbank_completed_orders():
+    redirect_to = check_session('BBSignIn')
+    if redirect_to:
+        return redirect_to
 
     sort_order = request.args.get('sort', 'desc')
     sort_direction = -1 if sort_order == 'desc' else 1
